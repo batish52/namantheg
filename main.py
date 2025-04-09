8f5c30f3ba8b0f3228506689aeee002dd07b9f17
batish52 <batish5nanu@gmail.com>
2025-04-09 23:45:23 +1000

    Update main.py

--- main.py
+++ main.py
@@ -27,5 +27,5 @@ def home():
     return "✅ NEURAL EDGE PROXY is LIVE via Fly.io"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 8080))  # Fly passes 8080
+    app.run(host="0.0.0.0", port=port)
